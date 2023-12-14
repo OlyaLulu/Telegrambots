11a6facae5e9c7b40d4bdc7af687db600af5524e
dilik <dilik.rayli@gmail.com>
2023-12-14 05:19:53 +0500

    creating telegram bots with aiogram library intermediate level

---
+++ intermediate/lesson-2/main.py
@@ -0,0 +1,23 @@
+import asyncio
+
+
+
+
+
+async def send_time(sec: int) -> None:
+    await asyncio.sleep(sec)
+    print(f'Прошло  {sec} секунд')
+
+
+print(send_time(2),send_time(5), sep='\n')
+
+
+async def main() -> None:
+    task_1 = asyncio.create_task(send_time(2))
+    task_2 = asyncio.create_task(send_time(5))
+
+    await task_1
+    await task_2
+
+if __name__ == '__main__':
+    asyncio.run(main())
